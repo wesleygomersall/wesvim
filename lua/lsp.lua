0a1834d6b3d3a39e87364004021a339937c6dd52
Wesley Gomersall <gomersallw@gmail.com>
2025-04-07 00:34:23 -0700

    Diagnostics in pop up menu

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,3 +7,11 @@ lsp.preset('recommended')
 lsp.nvim_workspace()
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
