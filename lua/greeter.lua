a56e35445dc1071c9973ad01a20950bff17f10e7
Wesley Gomersall <gomersallw@gmail.com>
2025-04-06 22:27:34 -0700

    Add lazy to options

--- lua/greeter.lua
+++ lua/greeter.lua
@@ -15,18 +15,12 @@ dashboard.section.header.val = {
 
 }
 
-
-
-
-
-
-
  dashboard.section.buttons.val = {
    dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
    dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
    dashboard.button("r", "Recently used files", ":Telescope oldfiles <CR>"),
    dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
-
+   dashboard.button("L", "Lazy package manager", ":Lazy <CR>"),
    dashboard.button("E", "Explore directory", ":Ex <CR>"),
    dashboard.button("q", "Quit Neovim", ":qa<CR>"),
 }
