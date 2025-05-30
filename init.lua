9fc11d1e0ce9098b20e8eaa2c2d25fab463e2dec
Wesley Gomersall <gomersallw@gmail.com>
2025-05-29 20:11:36 -0700

    native lsp support in nvim 0.11

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ require("options")
 require("lazy-config")
 require("bufferline").setup{}
 require("nvim-tree").setup{}
--- require("lsp") 
+require("lsp")
 require("autopair")
 require("greeter")
 
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,19 +1,3 @@
--- As of NVIM version 11.0, this is not required in init.lua
-
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
-
-lsp.setup()
-
-vim.diagnostic.config({
-  virtual_text = false
-})
-
--- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.lsp.enable('lua-language-server')
+vim.lsp.enable('bash-language-server')
+vim.lsp.enable('pyright')
