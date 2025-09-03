b46a4e838201d1d861c2e03bc9a521bc0a454285
Wesley Gomersall <gomersallw@gmail.com>
2025-09-02 17:27:14 -0700

    removed bad bad old old

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,7 +1,4 @@
 vim.cmd([[
-
     setlocal makeprg=pyright
     setlocal errorformat=%f:%l:%c\ -\ %t%.%#:%m
 ]])
-    --setlocal makeprg=python3\ %
-    --setlocal errorformat=%C\ %.%#,%A\ \ File\ \"%f\"\\,\ line\ %l%.%#,%Z%[%^\ ]%\\@=%m
