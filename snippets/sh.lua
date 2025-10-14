d67018a59aa5a23a5b8c1d3c72f5dce656aa1820
Wesley Gomersall <gomersallw@gmail.com>
2025-10-13 20:19:24 -0700

    slurm header added to bash snippets

--- snippets/sh.lua
+++ snippets/sh.lua
@@ -3,6 +3,23 @@
 return {
     -- manually triggered snippets
 
+    s({trig = "slurm", desc = "Slurm header"},
+        t({ '#SBATCH --account=',
+            '#SBATCH --job-name=',
+            '#SBATCH --output=logs/job_desc-%x_%j.out',
+            '#SBATCH --error=logs/job_desc-%x_%j.err',
+            '#SBATCH --partition=',
+            '#SBATCH --ntasks=1',
+            '#SBATCH --cpus-per-task=4',
+            '#SBATCH --time=12:00:00',
+            '#SBATCH --mem=16G',
+            '#SBATCH --mail-user=16G',
+            '#SBATCH --mem=16G',
+            '#SBATCH --mail-type=END',
+            '#SBATCH --mail-user=',
+        })
+    ),
+
 }, {
     -- autotriggered snippets
 
