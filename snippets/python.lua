f0c955da8e17c232980523efb62e2a7f50f97194
wesleygomersall <gomersallw@gmail.com>
2026-04-01 23:06:41 -0700

    snippet edits

--- snippets/python.lua
+++ snippets/python.lua
@@ -8,14 +8,25 @@ return {
             '# parser.add_argument("--input", "-i", type=str, default = "hello", help="")',
             '# parser.add_argument("--bool", "-b", action="store_true", default=False, help="")',
             '# parser.add_argument("--number", "-n", type=float, default = 3.14, help="")',
-            'args = parser.parse_args()'
-        })
+            'args = parser.parse_args()',
+            ''})
     ),
+
+    s({trig = "name main", desc = "if name is main, pass"},
+        t({'def main():',
+           '\tpass',
+           '',
+           'if __name__ == "__main__":',
+           '\tmain()',
+           ''})
+    ),
+
 
 }, {
     -- autotriggered snippets
 
 	s({trig = "#!/u", desc = "shebang"},
+<<<<<<< HEAD
         t("#!/usr/bin/env python3")
     ),
 
@@ -29,6 +40,9 @@ return {
 
 	s({trig = '"""', desc = '""" Docstring'},
         { t('"""'), i(1), t('"""') }
+=======
+        t({"#!/usr/bin/env python3", ""})
+>>>>>>> f17b7bc (snippet edits)
     ),
 
 }
--- snippets/sh.lua
+++ snippets/sh.lua
@@ -6,14 +6,12 @@ return {
     s({trig = "slurm", desc = "Slurm header"},
         t({ '#SBATCH --account=',
             '#SBATCH --job-name=',
-            '#SBATCH --output=logs/job_desc-%x_%j.out',
-            '#SBATCH --error=logs/job_desc-%x_%j.err',
+            '#SBATCH --output=logs/job-%x_%j.out',
+            '#SBATCH --error=logs/job-%x_%j.err',
             '#SBATCH --partition=',
             '#SBATCH --ntasks=1',
             '#SBATCH --cpus-per-task=4',
             '#SBATCH --time=12:00:00',
-            '#SBATCH --mem=16G',
-            '#SBATCH --mail-user=16G',
             '#SBATCH --mem=16G',
             '#SBATCH --mail-type=END',
             '#SBATCH --mail-user=',
@@ -23,8 +21,8 @@ return {
 }, {
     -- autotriggered snippets
 
-	s({trig = "#!/b", desc = "shebang"},
-        t("#!/bin/bash")
+	s({trig = "#!/", desc = "shebang"},
+        t({"#!/bin/bash", ""})
     ),
 
 }
